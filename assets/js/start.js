b68eba847fdb6d2e81140515212fcb8b409faa65
Lucy Kwon <ks02ks222@gmail.com>
2023-12-11 22:08:53 -0800

    changed styles

--- assets/js/start.js
+++ assets/js/start.js
@@ -1,19 +1,24 @@
 var time = dayjs().format('hh : mm A');
-var greeting = document.getElementById("intro");
-
+var greeting = document.getElementById("name");
+var weather = document.getElementById("weather");
+var intro = document.getElementById("intro");
 
 getname();
 function getname()  {
     var searchParams = document.location.search.split("q=");
     var user = searchParams[1]
-    intro(user);
+    getintro(user);
 }
 
-function intro(user)    {
+function getintro(user)    {
     var daynight = dayjs().format('A');
     var morning = dayjs().format('h');
     var time = parseInt(morning);
+    var userinfo = JSON.parse(localStorage.getItem(user));
+    var currentW = userinfo.weather;
+    var userCity = userinfo.city;
     
+
     if (daynight == "AM") {
         greeting.textContent = "Good Morning, " + user;
     }else if ((daynight == "PM") && (time < 6)){
@@ -21,11 +26,12 @@ function intro(user)    {
     } else if ((daynight == "PM") && (time > 6)){
         greeting.textContent = "Good Night, " + user;
     }
-    getUserInfo(user);
+
+    weather.textContent = "It is currently " + currentW + " In " + userCity;
+
+
+
 }
 
 
-function getUserInfo(user)  {
-    var userinfo = JSON.parse(localStorage.getItem(user));
-    console.log(userinfo);
-}
+   
